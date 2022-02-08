9ad4e16b82f4ccfe9ef990c46080a1e1e2e736ba
venkatanil96 <99185056+venkatanil96@users.noreply.github.com>
2022-02-08 11:05:47 +0530

    Add files via upload

---
+++ Celsius.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    class Celsius
+    {
+        static void Main(string[] args)
+        {
+             Console.Write("Enter the Celsius: ");
+            int celsius = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Kelvin = {0}", celsius + 273);
+            Console.WriteLine("Fahrenheit = {0}", celsius * 18 / 10 + 32);
+        }
+    }
+}
