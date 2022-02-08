d15f67d0d7d185eb5ab2309d33443aa7319d4b95
venkatanil96 <99185056+venkatanil96@users.noreply.github.com>
2022-02-08 11:12:05 +0530

    Add files via upload

---
+++ TableData.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Data;
+
+namespace assignment
+{
+    class tabledata
+    {
+        class DataTableProgram
+        {
+            public static DataTable CSVtoDataTable(string strFilePath)
+            {
+                DataTable dt = new DataTable();
+                using (StreamReader SR = new StreamReader(strFilePath))
+                {
+                    string[] headers = SR.ReadLine().Split(',');
+                    foreach (string header in headers)
+                    {
+                        dt.Columns.Add(header);
+                    }
+                    while (!SR.EndOfStream)
+                    {
+                        string[] rows = SR.ReadLine().Split(',');
+                        DataRow dr = dt.NewRow();
+                        for (int i = 0; i < headers.Length; i++)
+                        {
+                            dr[i] = rows[i];
+                        }
+                        dt.Rows.Add(dr);
+                    }
+
+                }
+
+
+                return dt;
+            }
+        }
+    }
+}
