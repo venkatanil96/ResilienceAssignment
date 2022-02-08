1a0d6846e0c71b48aaeb0366d977e48e8d3a45cf
venkatanil96 <99185056+venkatanil96@users.noreply.github.com>
2022-02-08 11:11:07 +0530

    Add files via upload

---
+++ SumOfEvenOdd.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    class SumOfEvenOdd
+    {
+        static void Main()
+        {
+            
+            EvenAndOdd();
+            Console.ReadLine();
+        }
+        static void EvenAndOdd()
+        {
+            int evenresult = 0;
+            int oddresult = 0;
+            int[] arr = new int[10];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine($"Enter the number{i + 1}");
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            foreach (int j in arr)
+            {
+                if (j % 2 == 0)
+                {
+                    evenresult = j + evenresult;
+                }
+                else
+                {
+                    oddresult = j + oddresult;
+                }
+            }
+            Console.WriteLine($"Sum of Even result is {evenresult} and Sum of Odd result is {oddresult}");
+        }
+    }
+}
