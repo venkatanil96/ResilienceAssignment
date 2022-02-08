bd39828f723ec2daadb9a8391d5230b5d8130bec
venkatanil96 <99185056+venkatanil96@users.noreply.github.com>
2022-02-08 11:06:25 +0530

    Add files via upload

---
+++ CircleOfCylinder.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    class CircleOfCylinder
+    {
+        public class Circle
+        {
+            public double radius;
+
+
+            public Circle(double r)
+            {
+                radius = r;
+
+
+            }
+
+
+            public double getArea()
+            {
+                return Math.PI * radius * radius;
+            }
+        }
+
+        public class Cylinder
+        {
+            public Circle dt;
+            public double height;
+            public string color;
+
+            public Cylinder(double r, double h, string c)
+            {
+                dt = new Circle(r);
+                height = h;
+            }
+            public Cylinder(double r, double h)
+            {
+                dt = new Circle(r);
+                height = h;
+            }
+            public Cylinder(double r)
+            {
+                dt = new Circle(r);
+
+            }
+
+
+
+            public double getVolume()
+            {
+                return dt.getArea() * height;
+            }
+
+
+        }
+
+        static void Main(string[] args)
+        {
+           
+
+            Cylinder cylinders = new Cylinder(12.34, 10.0, "white");
+
+
+            Cylinder[] circles = {
+                                new Cylinder(80),
+
+                            new Cylinder(12.3,30.7),
+                                    new Cylinder(12.34, 10.0, "white")
+                };
+
+            Console.WriteLine(circles[1].getVolume());
+            //Console.WriteLine(cylinders.getVolume());
+
+             Console.ReadLine();
+        }
+    }
+}
