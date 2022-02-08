b5a1994941beed4eccc0b9c2819bafcbe2e153cf
venkatanil96 <99185056+venkatanil96@users.noreply.github.com>
2022-02-08 11:10:44 +0530

    Add files via upload

---
+++ StarPattern.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    class StarPattern
+    {
+        public static void star()
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+        }       
+        static void Main(string[] args)
+        {
+            
+            star();
+            
+            Console.ReadLine();
+        }
+    }
+}
