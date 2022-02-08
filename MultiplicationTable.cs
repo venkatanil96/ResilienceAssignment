e108330b76f05406698eec9d517d8e5cb576e53b
venkatanil96 <99185056+venkatanil96@users.noreply.github.com>
2022-02-08 11:09:15 +0530

    Add files via upload

---
+++ MultiplicationTable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    class MultiplicationTable
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter the Number:");
+            int Tno = int.Parse(Console.ReadLine());
+            int i = 1;
+            while (i <= 10)
+            {
+                Console.WriteLine(Tno + " X " + i + " = " + Tno * i);
+                i++;
+            }
+        }
+    }
+}
